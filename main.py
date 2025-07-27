b7e2d78039bf13c373c55c735c5b1fd711d1a5a7
Alexey Ershov <aa.ershov@vkteam.ru>
2025-07-27 03:35:54 +0300

    fix: replace const path by sys.argv, refactor

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-from stats import get_words_count, get_chars_count, get_sorted_chars_dicts
-
-
-def get_book_text(path):
-	with open(path) as file:
-		return file.read()
+import sys
+from stats import get_words_count, get_chars_count, get_sorted_chars_dicts, generate_report, get_sorted_dicts_list
 	
 
-def main(path):
+def main():
+    check_args()
+    path = sys.argv[1]
     text = get_book_text(path)
     sorted_dict_of_chars = get_sorted_chars_dicts(get_chars_count(text))
     
     char_lines = []
-    for item in sorted_dict_of_chars:
-        char = item["char"]
-        if not char.isalpha():
-            continue
-        char_lines.append(f"{char}: {item['num']}")
+    get_sorted_dicts_list(sorted_dict_of_chars, char_lines)
 
     joined_output = "\n".join(char_lines)
     
-    print(f'''
-	============ BOOKBOT ============
-	Analyzing book found at books/frankenstein.txt...
-	----------- Word Count ----------
-	Found {get_words_count(text)} total words
-	--------- Character Count -------
-    {joined_output}
-	============= END ===============
-    ''')
+    generate_report(path, text, joined_output)
+    
+
+def get_book_text(path):
+	with open(path) as file:
+		return file.read()
+     
+
+def check_args():
+	if len(sys.argv) < 2:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
 
 
-main("./books/frankenstein.txt")
+main()
--- stats.py
+++ stats.py
@@ -21,3 +21,24 @@ def get_sorted_chars_dicts(dictionary):
 	for key in dictionary:
 		chars_nums_list.append({"char": key, "num": dictionary[key]})
 	return sorted(chars_nums_list, reverse=True, key=sort_on)
+
+
+def get_sorted_dicts_list(dictionary, result_list):
+	char_lines = []
+	for item in dictionary:
+		char = item["char"]
+		if not char.isalpha():
+			continue
+		result_list.append(f"{char}: {item['num']}")
+
+
+def generate_report(path, text, joined_output):
+	print(f'''
+	============ BOOKBOT ============
+	Analyzing book found at {path}...
+	----------- Word Count ----------
+	Found {get_words_count(text)} total words
+	--------- Character Count -------
+    {joined_output}
+	============= END ===============
+    ''')
